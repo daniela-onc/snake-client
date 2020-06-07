bd91a133b16f6bb633192f56f144cfa69085814c
Daniela <danielaoancea2009@yahoo.ca>
2020-06-07 11:05:59 -0400

    Update play.js

--- play.js
+++ play.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 const { connect } = require('./client');
-=======
- const { connect } = require('./client');
->>>>>>> 923ee07f2fabae28a7f4c5ceca510606c0331c65
 const { setupInput } = require('./input');
 
 console.log('Connecting ...');
-let conn = connect();
 
-/**
- * Setup User Interface
- * Specifically, so that we can handle user input via stdin
- */
-
-setupInput(conn);
+setupInput(connect())
+module.exports = { connect }
