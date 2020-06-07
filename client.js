1d84ae23e91080537752d671f7297c4ead4bb4d2
Daniela <danielaoancea2009@yahoo.ca>
2020-06-07 11:04:57 -0400

    Update client.js

--- client.js
+++ client.js
@@ -3,30 +3,31 @@ const { IP, PORT } = require('./constants');
 
 const connect = function () {
   const conn = net.createConnection({
-    host: IP,
-    port: PORT
+    host: '135.23.222.148',
+    port: 50541
+    });
+  
+
+  conn.on('data', function(data) {
+    console.log(data.toString());
+    conn.end();
   });
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-  conn.on('connect', () => {
+
+  conn.on('connect', function() {
     console.log('Successfully connected to game server');
-    conn.write('Name: ABC');
-    for (let x = 0; x < 10; x++) {
-      setTimeout(() => {
-        conn.write('Move: up');
-      }, 100 * x);
-    }
   });
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
+
+
+  conn.on('connect', function () {
+    conn.write("Name: DAN");
+
   });
+
+  
+  //interpret incoming data as text
+  conn.setEncoding('utf8'); 
 
   return conn;
-};
-
-module.exports = { connect };
-<<<<<<< HEAD
-
-=======
->>>>>>> 923ee07f2fabae28a7f4c5ceca510606c0331c65
+}
+module.exports = { connect }
 
